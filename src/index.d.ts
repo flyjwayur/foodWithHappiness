6050747de99b9f1693c0c86acd65252eb9e77895
HyeSoo <flyjwayur@gmail.com>
2020-02-07 12:36:47 +0200

    Set up the storybook & styled-components and fix formatting

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,21 +1,20 @@
 export interface IDataRestaurant {
-  blurhash : string;
-  city: string,
-  currency: string,
-  delivery_price: number,
-  description: string,
-  image: string,
-  location: number[],
-  name: string,
-  online: boolean,
-  tags: string[]
+  blurhash: string;
+  city: string;
+  currency: string;
+  delivery_price: number;
+  description: string;
+  image: string;
+  location: number[];
+  name: string;
+  online: boolean;
+  tags: string[];
 }
 
 interface IPropRestaurant {
-  name: string,
-  description: string,
-  delivery_price: number,
-  blurhash: string,
-  image: string,
+  name: string;
+  description: string;
+  delivery_price: number;
+  blurhash: string;
+  image: string;
 }
-
---
+++ src/theme/globalStyle.ts
@@ -0,0 +1,14 @@
+import { createGlobalStyle, css } from "styled-components";
+
+export const bodyStyles = css`
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+  font-family: "Karla", Roboto, Helvetica Neue, sans-serif;
+  font-size: 1.3rem;
+`;
+
+export const GlobalStyle = createGlobalStyle`
+body {
+   ${bodyStyles}
+}`;
