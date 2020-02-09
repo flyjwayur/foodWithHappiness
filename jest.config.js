41884c9601bf033139f688c87f933b4d1eb095c6
HyeSoo <flyjwayur@gmail.com>
2020-02-09 21:51:38 +0200

    Test snapshot of App
    By testing App, the gain of the test coverage is the most compared to testing any other component.
    By testing Snapshot, we will detect if there is any changes in UI.

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,15 @@
 module.exports = {
-  roots: ["<rootDir>/src"], // The root of the source code, a token Jest substitutes
+  roots: ['<rootDir>/src'], // The root of the source code, a token Jest substitutes
   transform: {
-    "^.+\\.tsx?$": "ts-jest" // Jest transformations, adds support for TS, using ts=jest
+    '^.+\\.tsx?$': 'ts-jest', // Jest transformations, adds support for TS, using ts=jest
+    '.+\\.(css|styl|less|sass|scss|png|jpg|ttf|woff|woff2)$': 'jest-transform-stub',
+    '^.+\\.js$': 'babel-jest'
   },
+  transformIgnorePatterns: ['<rootDir>/node_modules/'],
   setupFilesAfterEnv: [
-    "@testing-library/jest-dom/extend-expect"
+    '@testing-library/react/cleanup-after-each',
+    '@testing-library/jest-dom/extend-expect'
   ], // Extended assertions when using React Testing Library
-  testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$", // Test folder/file pattern
-  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"] // module file extensions for importing
+  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$', // Test folder/file pattern
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'] // module file extensions for importing
 };
